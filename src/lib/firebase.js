99945ab343f833fc56e674414e1ea11ffa7cffc0
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-09-30 12:53:30 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,26 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// replace these with your own config values from Firebase!
+const firebaseconfig = {
+  apiKey: 'AIzaSyCAw3yXjcam7HJaS0ThALIMoZ_ICUR9V1k',
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCAw3yXjcam7HJaS0ThALIMoZ_ICUR9V1k",
-  authDomain: "insta-536de.firebaseapp.com",
-  projectId: "insta-536de",
-  storageBucket: "insta-536de.appspot.com",
-  messagingSenderId: "859496964055",
-  appId: "1:859496964055:web:8a1ddf6dc33cbef83a3592",
-  measurementId: "G-SVXSQ39P6L"
+  authDomain: 'insta-536de.firebaseapp.com',
+
+  projectId: 'insta-536de',
+
+  storageBucket: 'insta-536de.appspot.com',
+
+  messagingSenderId: '859496964055',
+
+  appId: '1:859496964055:web:8a1ddf6dc33cbef83a3592',
+
+  measurementId: 'G-SVXSQ39P6L'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebase = Firebase.initializeApp(firebaseconfig);
+const { FieldValue } = Firebase.firestore;
+
+export { firebase, FieldValue };
+
