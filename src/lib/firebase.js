cf98988ea8fa40d5f9d790b366250e50f0f8d626
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-09-30 11:11:23 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,19 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCBpRfjMXI0uQ8obfVsUkGDy5jMzorakUY',
-  authDomain: 'instagram-yt.firebaseapp.com',
-  projectId: 'instagram-yt',
-  storageBucket: 'instagram-yt.appspot.com',
-  messagingSenderId: '763311512694',
-  appId: '1:763311512694:web:fc66f0787342163c791357'
+  apiKey: 'AIzaSyCAw3yXjcam7HJaS0ThALIMoZ_ICUR9V1k',
+
+  authDomain: 'insta-536de.firebaseapp.com',
+
+  projectId: 'insta-536de',
+
+  storageBucket: 'insta-536de.appspot.com',
+
+  messagingSenderId: '859496964055',
+
+  appId: '1:859496964055:web:8a1ddf6dc33cbef83a3592',
+
+  measurementId: 'G-SVXSQ39P6L'
 };
 
 const firebase = Firebase.initializeApp(config);
